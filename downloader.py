b3ae27c9ef15dbbd4b9714c6d9aee505a30dc7bd
Md. Zahidul Islam <vdjsovaj@gmail.com>
2022-01-23 11:31:46 +0600

    updated

--- downloader.py
+++ downloader.py
@@ -11,19 +11,78 @@ def video_Info(yt):
     print("Thumbnail Url: ", yt.thumbnail_url)
     print(yt.description)
 
+
+def download_Video(yt):
+    my_streams = yt.streams.filter(file_extension='mp4',only_video= True)
+    for streams in my_streams:
+        print(f"Video Tag : {streams.itag} Resolution : {streams.resolution}")
+
+    input_itag = input("Enter itag Value: ")
+
+    video = yt.streams.get_by_itag(input_itag)
+
+    video.download()
+    print("Video is Downloading as ", yt.title , ".mp4")
+
+
+
+def download_Audio(yt):
+    my_streams = yt.streams.filter(only_audio= True)
+    for streams in my_streams:
+        print(f"Audio Tag : {streams.itag} Resolution : {streams.abr}")
+
+    input_itag = input("Enter itag Value: ")
+
+    audio = yt.streams.get_by_itag(input_itag)
+
+    audio.download()
+    print("Audio is Downloading as ", yt.title , ".mp3")
+
+
+
 def downloadPlayList(p):
     #print Playlist title....
     print(p.title)
 
-    # for v in p.video_Info:
-    #     try:
-            
-    #     except Exception as e:
-    #         print(e,type(e))
+    for v in p.videos:
+        try:
+            # Download all videos in best resolution
+            v.streams.first().download(v.title)
+        except Exception as e:
+            print(e,type(e))
 
     print("Playlist is Downoaded")
 
-link = input()
-yt = YouTube(link)
 
-video_Info(yt)
+while True:
+    print("1. Youtube Video Download")
+    print("2. Youtube Audio Download")
+    print("3. Youtube Playlist Download")
+    print("4. Video Info")
+    print("5. Exit")
+
+    op = int(input())
+
+    if op == 5:
+        break
+    elif op == 1:
+        print("Enter Video Link: ")
+        link = input()
+        yt = YouTube(link)
+        download_Video(yt)
+    elif op == 2:
+        print("Enter Video Link: ")
+        link = input()
+        yt = YouTube(link)
+        download_Audio(yt)
+    elif op == 3:
+        print("Enter Playlist Link: ")
+        link = input()
+        yt = YouTube(link)
+        downloadPlayList(yt) 
+
+    elif op == 4:
+        print("Enter Video Link: ")
+        link = input()
+        yt = YouTube(link)
+        video_Info(yt)  
